b0abdd42d06fb7dd5ca5ebdfc0691d0edc5163af
abrsh7 <abrshn7@gmail.com>
2023-08-08 10:04:38 -0700

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,21 @@
+#!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    
-    # Set a custom User-Agent to avoid "Too Many Requests" error
-    headers = {"User-Agent": "Custom User Agent"}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            posts = data["data"]["children"]
-            
-            if len(posts) == 0:
-                print("No posts found.")
-            else:
-                for post in posts:
-                    title = post["data"]["title"]
-                    print(title)
-        else:
-            print("Invalid subreddit.")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
